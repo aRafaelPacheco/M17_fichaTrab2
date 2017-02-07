72a45dc2b43229f1f4564151a4a1f6df0fbdc1d7
João Barbosa <Joo Barbosa@asuspc>
2017-02-07 11:58:10 +0000

    Projeto 01 - teste ao GitHub

---
+++ src/m17b_fichatrab2/Main.java
@@ -0,0 +1,16 @@
+package m17b_fichatrab2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Ficha de Trabalho #2
+        Ponto2D p1 = new Ponto2D(5,7);
+        Ponto2D p2 = new Ponto2D(2,8);
+        
+        System.out.println(p1);
+        System.out.println(p2);
+        
+        System.out.println(p2.distancia(p1));
+    }
+    
+}
---
+++ src/m17b_fichatrab2/Ponto2D.java
@@ -0,0 +1,16 @@
+package m17b_fichatrab2;
+
+public class Ponto2D {
+    private int x;
+    private int y;
+    
+    public Ponto2D(int x, int y){
+        this.x = x;
+        this.y = y;        
+    }
+    
+    public double distancia(Ponto2D a){
+        return 0.0;
+    }
+    
+}
