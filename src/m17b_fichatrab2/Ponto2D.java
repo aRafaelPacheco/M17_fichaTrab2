44ba24dd79c4dd6b586ffe62f9c325d3dda9eea5
João Barbosa <Joo Barbosa@asuspc>
2017-02-07 12:05:13 +0000

    Ficha de Trabalho #2

--- src/m17b_fichatrab2/Ponto2D.java
+++ src/m17b_fichatrab2/Ponto2D.java
@@ -4,12 +4,15 @@ public class Ponto2D {
     private int x;
     private int y;
     
+    // Faltam construtor, getters e setters.
+    
     public Ponto2D(int x, int y){
         this.x = x;
         this.y = y;        
     }
     
     public double distancia(Ponto2D a){
+        // Falta fazer este método.
         return 0.0;
     }
     
